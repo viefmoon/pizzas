5c9ee62f08b34d568a50cd4bf542fd5640a63fe2
Leonel <viefmoon@gmail.com>
2025-04-08 14:38:14 -0600

    Implementa la navegación en la aplicación utilizando React Navigation, añadiendo un stack navigator para la pantalla de inicio de sesión. Se actualiza la configuración de las variables de entorno y se agrega la dependencia dotenv. Se eliminan dependencias innecesarias en package.json y package-lock.json.

--- App.tsx
+++ App.tsx
@@ -1,15 +1,20 @@
 import React, { useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import { PaperProvider } from "react-native-paper";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useThemeStore, useSystemThemeDetector } from "./src/store/themeStore";
 import GlobalSnackbar from "./src/components/common/GlobalSnackbar";
 import LoginScreen from "./src/screens/auth/LoginScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 // Crear una instancia de QueryClient para React Query
 const queryClient = new QueryClient();
+
+// Crear el stack navigator
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   // Usar el tema global de la aplicación
@@ -23,14 +28,20 @@ export default function App() {
     <SafeAreaProvider>
       <QueryClientProvider client={queryClient}>
         <PaperProvider theme={activeTheme}>
-          {/* Ajustar StatusBar según el tema */}
-          <StatusBar style={isDarkMode ? "light" : "dark"} />
+          <NavigationContainer>
+            <Stack.Navigator>
+              <Stack.Screen
+                name="Login"
+                component={LoginScreen}
+                options={{ headerShown: false }}
+              />
+            </Stack.Navigator>
+            {/* Ajustar StatusBar según el tema */}
+            <StatusBar style={isDarkMode ? "light" : "dark"} />
 
-          {/* Contenido principal de la aplicación */}
-          <LoginScreen />
-
-          {/* Snackbar global accesible desde cualquier parte */}
-          <GlobalSnackbar />
+            {/* Snackbar global accesible desde cualquier parte */}
+            <GlobalSnackbar />
+          </NavigationContainer>
         </PaperProvider>
       </QueryClientProvider>
     </SafeAreaProvider>
