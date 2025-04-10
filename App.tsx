9ba226a89d9b8bab3c3123385501ccbfdafc0947
Leonel <viefmoon@gmail.com>
2025-04-09 21:25:28 -0600

    Refactoriza el componente App para mejorar la claridad del código. Se actualizan los comentarios para reflejar mejor la funcionalidad de cada sección, incluyendo la inicialización de autenticación y la detección del tema del sistema. Se optimiza la legibilidad del código al eliminar comentarios redundantes.

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { ActivityIndicator, View } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { useThemeStore, useSystemThemeDetector } from './src/app/store/themeStore'; // Importación original
+import { useThemeStore, useSystemThemeDetector } from './src/app/store/themeStore';
 import { AppNavigator } from './src/app/navigation/AppNavigator';
 import GlobalSnackbar from './src/app/components/common/GlobalSnackbar';
 import { useInitializeAuth } from './src/app/hooks/useInitializeAuth';
@@ -11,26 +11,22 @@ import { useInitializeAuth } from './src/app/hooks/useInitializeAuth';
 const queryClient = new QueryClient();
 
 export default function App() {
-  // Usar el hook de inicialización de autenticación
   const isInitializingAuth = useInitializeAuth();
 
-  // Llama al hook para detectar y actualizar el tema del sistema en el store ANTES de leer el tema
-  useSystemThemeDetector();
+  useSystemThemeDetector(); // Detecta y actualiza el tema del sistema en el store
 
-  // Leer activeTheme DESPUÉS de llamar al detector
-  const activeTheme = useThemeStore((state) => state.activeTheme);
+  const activeTheme = useThemeStore((state) => state.activeTheme); // Lee el tema activo del store
 
-  // Mostrar pantalla de carga mientras se inicializa la autenticación
+  // Muestra pantalla de carga durante la inicialización de autenticación
   if (isInitializingAuth) {
     return (
-      // Usar un View simple para la carga
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: activeTheme.colors.background }}>
         <ActivityIndicator size="large" color={activeTheme.colors.primary} />
       </View>
     );
   }
 
-  // Una vez inicializado, renderizar la app principal
+  // Renderiza la app principal una vez inicializada la autenticación
   return (
     <SafeAreaProvider>
       <QueryClientProvider client={queryClient}>
